560ab7f571d4e408c6e0b6caab5f74f5c87523db
Gary <gary@codesbiome.com>
2020-05-22 06:32:55 +0530

    Created webpack helpers function file

---
+++ tools/webpack/webpack.helpers.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+const path = require('path');
+const cwd = process.cwd();
+
+// Creates Webpack Aliases using CWD path
+const createWebpackAliases = (als) => {
+  const result = {};
+  for (const name in als) {
+    result[name] = path.join(cwd, als[name]);
+  }
+  console.log(result);
+  return result;
+};
+
+// Export webpack helpers
+module.exports = {
+  createWebpackAliases,
+};
