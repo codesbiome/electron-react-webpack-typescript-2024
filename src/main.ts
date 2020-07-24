6addf745ff621952e69c6949d61a85a42d0c2593
Gary <gary@codesbiome.com>
2020-07-24 11:35:49 +0530

    Added webPreferences for window

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { app, BrowserWindow } from 'electron';
+
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
+declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -10,13 +12,17 @@ if (require('electron-squirrel-startup')) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
     width: 800,
     height: 600,
     backgroundColor: '#242424',
     show: false,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
+      enableRemoteModule: false,
+      contextIsolation: true,
+      nodeIntegrationInWorker: false,
+      nodeIntegrationInSubFrames: false,
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
   });
 
