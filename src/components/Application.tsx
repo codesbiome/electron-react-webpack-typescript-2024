07994688aff295bc3570f218ec2832d1005b67dc
codesbiome <github@codesbiome.com>
2021-08-17 14:28:35 +0530

    Overhaul application layout and style

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -15,17 +15,19 @@ const Application: React.FC<Props> = (props) => {
     <React.Fragment>
       <main>
         <div className='main-heading'>
-          <img src={logo} width='32' title='Codesbiome' />
           <h1>{props.title}</h1>
         </div>
-        <p className='main-teaser'>
-          Custom boilerplate for rapid development of Desktop Applications.
-          <br />
-          This project makes use of Electron, React, Typescript, Webpack to
-          serve the best environment for development with hot-reloading of
-          modules.
-        </p>
-        <div className='versions'>
+        <div className='main-teaser'>
+          <img src={logo} title='Codesbiome' />
+          <div>
+            Minimal boilerplate for rapid development of Desktop Applications.
+            <br />
+            This project makes use of Electron, React, Typescript, Webpack to
+            serve the best environment for development with hot-reloading of
+            modules and styles.
+          </div>
+        </div>
+        <div className='versions center'>
           <span>
             ERWT <span>{props.version}</span>
           </span>
@@ -39,15 +41,21 @@ const Application: React.FC<Props> = (props) => {
             Node <span id='node-version'></span>
           </span>
         </div>
-        <p className='main-teaser small'>
-          Click below button to update the application &quot;counter&quot;
-          state. Components will update their states using
-          Hot-Module-Replacement technique, without needing to refresh/reload
-          whole application.
+        <p className='main-teaser small center'>
+          Click &quot;Counter&quot; button(s) to update the app
+          &quot;counter&quot; state. Components will update their states using
+          Hot-Module-Replacement (HMR) without needing to refresh/reload whole
+          application.
         </p>
-        <button onClick={() => setCounter(counter + 1)}>
-          Counter <span>{counter}</span>
-        </button>
+        <div className='center'>
+          <button onClick={() => setCounter(counter + 1)}>
+            Increment Counter <span>{counter}</span>
+          </button>
+          &nbsp;&nbsp; &nbsp;&nbsp;
+          <button onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>
+            Decrement Counter <span>{counter}</span>
+          </button>
+        </div>
       </main>
     </React.Fragment>
   );
