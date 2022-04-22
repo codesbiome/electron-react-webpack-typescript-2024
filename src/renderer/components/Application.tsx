43a04de942260fb7cab660e1cc96afd431bb30c8
codesbiome <github@codesbiome.com>
2022-04-22 13:53:33 +0530

    Update application to implement new layout with theme modes

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,24 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 import logo from '@assets/images/logo.png';
-import './Application.less';
+import darkModeIcon from '@assets/images/darkmode.png';
+import lightModeIcon from '@assets/images/lightmode.png';
+import './Application.scss';
 
 const Application: React.FC = () => {
   const [counter, setCounter] = useState(0);
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  /**
+   * On component mount
+   */
+  useEffect(() => {
+    const useDarkTheme = parseInt(localStorage.getItem('dark-mode'));
+    if (isNaN(useDarkTheme)) {
+      setDarkTheme(true);
+    } else if (useDarkTheme == 1) {
+      setDarkTheme(true);
+    } else if (useDarkTheme == 0) {
+      setDarkTheme(false);
+    }
+  }, []);
+
+  /**
+   * On Dark theme change
+   */
+  useEffect(() => {
+    if (darkTheme) {
+      localStorage.setItem('dark-mode', '1');
+      document.body.classList.add('dark-mode');
+    } else {
+      localStorage.setItem('dark-mode', '0');
+      document.body.classList.remove('dark-mode');
+    }
+  }, [darkTheme]);
+
+  /**
+   * Toggle Theme
+   */
+  function toggleTheme() {
+    setDarkTheme(!darkTheme);
+  }
 
   return (
     <div id='erwt'>
       <div className='header'>
         <div className='main-heading'>
-          <h1>ERWT Boilerplate</h1>
+          <h1 className='themed'>ERWT - Electron Boilerplate</h1>
         </div>
         <div className='main-teaser'>
           <img src={logo} title='Codesbiome' />
           <div>
             Minimal boilerplate for rapid development of Desktop Applications
-            using Electron, React, Typescript and Webpack. To serve the best
-            environment for development, it uses hot-reloading of modules,
-            styles and layouts of project
+            using Electron, React, Typescript and Webpack. For faster
+            development experience, this application will update using Hot
+            Reload without needing to restart.
           </div>
         </div>
         <div className='versions'>
@@ -38,18 +75,30 @@ const Application: React.FC = () => {
       </div>
 
       <div className='footer'>
-        <p className='main-teaser small center'>
-          For faster development experience, this application will update using
-          Hot Reload without needing to restart/reload after code changes. Click
-          below buttons to update the application &quot;counter&quot; state
-        </p>
         <div className='center'>
-          <button onClick={() => setCounter(counter + 1)}>
-            Increment Counter <span>{counter}</span>
+          <button
+            onClick={() => {
+              if (counter > 99) return alert('Going too high!!');
+              setCounter(counter + 1);
+            }}
+          >
+            Increment <span>{counter}</span>
           </button>
           &nbsp;&nbsp; &nbsp;&nbsp;
-          <button onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>
-            Decrement Counter <span>{counter}</span>
+          <button
+            onClick={() => {
+              if (counter == 0) return alert('Oops.. thats not possible!');
+              setCounter(counter > 0 ? counter - 1 : 0);
+            }}
+          >
+            Decrement <span>{counter}</span>
+          </button>
+          &nbsp;&nbsp; &nbsp;&nbsp;
+          <button onClick={toggleTheme}>
+            {darkTheme ? 'Light Mode' : 'Dark Mode'}
+            <span>
+              <img src={darkTheme ? lightModeIcon : darkModeIcon} />
+            </span>
           </button>
         </div>
       </div>
