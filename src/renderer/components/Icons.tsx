64d06f1341de38e89b87edda52f51e3fda545e2f
yonatanmgr <yonatanmgr@gmail.com>
2023-02-28 11:14:26 +0200

    fixed logo

--- src/renderer/components/Icons.tsx
+++ src/renderer/components/Icons.tsx
@@ -1,5 +1,7 @@
 import erwt from '@assets/icons/erwt.png';
+import LogoIcon from '@assets/icons/logo.svg';
 
 export const icons = {
-  erwt
+  erwt,
+  LogoIcon
 };
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -2,3 +2,4 @@ declare module '*.css';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
+declare module '*.svg';
