62879213620c4871c09f2c13916c614e1cdae58d
codesbiome <codesbiome@gmail.com>
2024-01-10 17:09:51 +0530

    Remove unused misc directory

--- misc/window/windowPreload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge } from 'electron';
-import titlebarContext from './titlebarContext';
-
-contextBridge.exposeInMainWorld('electron_window', {
-  titlebar: titlebarContext,
-});
